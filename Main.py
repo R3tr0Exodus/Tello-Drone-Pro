0792c60a5c0d1dd358737b3f2f1136ac07109b0e
R3tr0 <toby@soegaards.dk>
2023-08-24 12:31:06 +0200

    Updated example

--- Main.py
+++ Main.py
@@ -1,9 +1,14 @@
-
+import cv2
 from djitellopy import Tello
 
 tello = Tello()
 
 tello.connect()
+tello.streamon()
+frame_read = tello.get_frame_read()
+
 tello.takeoff()
+tello.move_up(160)
+cv2.imwrite("GangGang.png", frame_read.frame)
 
 tello.land()
