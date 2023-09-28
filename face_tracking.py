ae24182588ee76372037e01e827dca8c6c8f1110
MathiTC <mtcskole@gmail.com>
2023-09-28 12:18:20 +0200

    working facetrack

--- face_tracking.py
+++ face_tracking.py
@@ -1,221 +1,103 @@
 from djitellopy import Tello
 import cv2
 import numpy as np
-
-# Import face detection libraries
 import dlib
 from imutils import face_utils
 
 # Initialize the face detector and facial landmarks predictor
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+predictor = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")
 
-######################################################################
-width = 640  # WIDTH OF THE IMAGE
-height = 480  # HEIGHT OF THE IMAGE
-deadZone = 100
-######################################################################
+# Constants
+width = 640
+height = 480
+deadZone = 50  # Adjust this dead zone as needed
 
-startCounter = 0
-
-# CONNECT TO TELLO
+# Connect to Tello
 me = Tello()
 me.connect()
-me.for_back_velocity = 0
-me.left_right_velocity = 0
-me.up_down_velocity = 0
-me.yaw_velocity = 0
-me.speed = 0
 
-print(me.get_battery())
-
+# Start video stream
 me.streamoff()
 me.streamon()
-########################
 
-frameWidth = width
-frameHeight = height
+# Take off
+me.takeoff()
 
-global imgContour
-global dir;
-
-
-def empty(a):
-    pass
-
-
-cv2.namedWindow("HSV")
-cv2.resizeWindow("HSV", 640, 240)
-cv2.createTrackbar("HUE Min", "HSV", 20, 179, empty)
-cv2.createTrackbar("HUE Max", "HSV", 40, 179, empty)
-cv2.createTrackbar("SAT Min", "HSV", 148, 255, empty)
-cv2.createTrackbar("SAT Max", "HSV", 255, 255, empty)
-cv2.createTrackbar("VALUE Min", "HSV", 89, 255, empty)
-cv2.createTrackbar("VALUE Max", "HSV", 255, 255, empty)
-
-cv2.namedWindow("Parameters")
-cv2.resizeWindow("Parameters", 640, 240)
-cv2.createTrackbar("Threshold1", "Parameters", 166, 255, empty)
-cv2.createTrackbar("Threshold2", "Parameters", 171, 255, empty)
-cv2.createTrackbar("Area", "Parameters", 1750, 30000, empty)
-
-
-def stackImages(scale, imgArray):
-    rows = len(imgArray)
-    cols = len(imgArray[0])
-    rowsAvailable = isinstance(imgArray[0], list)
-    width = imgArray[0][0].shape[1]
-    height = imgArray[0][0].shape[0]
-    if rowsAvailable:
-        for x in range(0, rows):
-            for y in range(0, cols):
-                if imgArray[x][y].shape[:2] == imgArray[0][0].shape[:2]:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
-                else:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]),
-                                                None, scale, scale)
-                if len(imgArray[x][y].shape) == 2: imgArray[x][y] = cv2.cvtColor(imgArray[x][y], cv2.COLOR_GRAY2RGB)
-        imageBlank = np.zeros((height, width, 3), np.uint8)
-        hor = [imageBlank] * rows
-        hor_con = [imageBlank] * rows
-        for x in range(0, rows):
-            hor[x] = np.hstack(imgArray[x])
-        ver = np.vstack(hor)
-    else:
-        for x in range(0, rows):
-            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
-                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
-            else:
-                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None, scale,
-                                         scale)
-            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2RGB)
-        hor = np.hstack(imgArray)
-        ver = hor
-    return ver
-
-
-def getContours(img, imgContour):
-    global dir
-    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    for cnt in contours:
-        area = cv2.contourArea(cnt)
-        areaMin = cv2.getTrackbarPos("Area", "Parameters")
-        if area > areaMin:
-            cv2.drawContours(imgContour, cnt, -1, (255, 255, 0), 7)
-            peri = cv2.arcLength(cnt, True)
-            approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
-            # print(len(approx))
-            x, y, w, h = cv2.boundingRect(approx)
-            cx = int(x + (w / 2))  # CENTER X OF THE OBJECT
-            cy = int(y + (h / 2))  # CENTER X OF THE OBJECT
-
-            if (cx < int(frameWidth / 2) - deadZone):
-                cv2.putText(imgContour, " GO LEFT ", (20, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 3)
-                cv2.rectangle(imgContour, (0, int(frameHeight / 2 - deadZone)),
-                              (int(frameWidth / 2) - deadZone, int(frameHeight / 2) + deadZone), (0, 0, 255),
-                              cv2.FILLED)
-                dir = 1
-            elif (cx > int(frameWidth / 2) + deadZone):
-                cv2.putText(imgContour, " GO RIGHT ", (20, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 3)
-                cv2.rectangle(imgContour, (int(frameWidth / 2 + deadZone), int(frameHeight / 2 - deadZone)),
-                              (frameWidth, int(frameHeight / 2) + deadZone), (0, 0, 255), cv2.FILLED)
-                dir = 2
-            elif (cy < int(frameHeight / 2) - deadZone):
-                cv2.putText(imgContour, " GO UP ", (20, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 3)
-                cv2.rectangle(imgContour, (int(frameWidth / 2 - deadZone), 0),
-                              (int(frameWidth / 2 + deadZone), int(frameHeight / 2) - deadZone), (0, 0, 255),
-                              cv2.FILLED)
-                dir = 3
-            elif (cy > int(frameHeight / 2) + deadZone):
-                cv2.putText(imgContour, " GO DOWN ", (20, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 3)
-                cv2.rectangle(imgContour, (int(frameWidth / 2 - deadZone), int(frameHeight / 2) + deadZone),
-                              (int(frameWidth / 2 + deadZone), frameHeight), (0, 0, 255), cv2.FILLED)
-                dir = 4
-            else:
-                dir = 0
-
-            cv2.line(imgContour, (int(frameWidth / 2), int(frameHeight / 2)), (cx, cy), (0, 0, 255), 3)
-            cv2.rectangle(imgContour, (x, y), (x + w, y + h), (0, 255, 0), 5)
-            cv2.putText(imgContour, "Points: " + str(len(approx)), (x + w + 20, y + 20), cv2.FONT_HERSHEY_COMPLEX, .7,
-                        (0, 255, 0), 2)
-            cv2.putText(imgContour, "Area: " + str(int(area)), (x + w + 20, y + 45), cv2.FONT_HERSHEY_COMPLEX, 0.7,
-                        (0, 255, 0), 2)
-            cv2.putText(imgContour, " " + str(int(x)) + " " + str(int(y)), (x - 20, y - 45), cv2.FONT_HERSHEY_COMPLEX,
-                        0.7, (0, 255, 0), 2)
-        else:
-            dir = 0
-
-
-def display(img):
-    cv2.line(img, (int(frameWidth / 2) - deadZone, 0), (int(frameWidth / 2) - deadZone, frameHeight), (255, 255, 0), 3)
-    cv2.line(img, (int(frameWidth / 2) + deadZone, 0), (int(frameWidth / 2) + deadZone, frameHeight), (255, 255, 0), 3)
-    cv2.circle(img, (int(frameWidth / 2), int(frameHeight / 2)), 5, (0, 0, 255), 5)
-    cv2.line(img, (0, int(frameHeight / 2) - deadZone), (frameWidth, int(frameHeight / 2) - deadZone), (255, 255, 0),
-             3)
-    cv2.line(img, (0, int(frameHeight / 2) + deadZone), (frameWidth, int(frameHeight / 2) + deadZone), (255, 255, 0),
-             3)
-
-
+# Main loop
 while True:
-
-    # GET THE IMAGE FROM TELLO
+    # Get the frame from Tello
     frame_read = me.get_frame_read()
-    myFrame = frame_read.frame
-    img = cv2.resize(myFrame, (width, height))
+    frame = frame_read.frame
+    img = cv2.resize(frame, (width, height))
     imgContour = img.copy()
-    imgHsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
 
-    h_min = cv2.getTrackbarPos("HUE Min", "HSV")
-    h_max = cv2.getTrackbarPos("HUE Max", "HSV")
-    s_min = cv2.getTrackbarPos("SAT Min", "HSV")
-    s_max = cv2.getTrackbarPos("SAT Max", "HSV")
-    v_min = cv2.getTrackbarPos("VALUE Min", "HSV")
-    v_max = cv2.getTrackbarPos("VALUE Max", "HSV")
+    # Convert to grayscale for face detection
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    lower = np.array([h_min, s_min, v_min])
-    upper = np.array([h_max, s_max, v_max])
-    mask = cv2.inRange(imgHsv, lower, upper)
-    result = cv2.bitwise_and(img, img, mask=mask)
-    mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB)
+    # Detect faces in the frame
+    faces = detector(gray)
 
-    imgBlur = cv2.GaussianBlur(result, (7, 7), 1)
-    imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_RGB2GRAY)
-    threshold1 = cv2.getTrackbarPos("Threshold1", "Parameters")
-    threshold2 = cv2.getTrackbarPos("Threshold2", "Parameters")
-    imgCanny = cv2.Canny(imgGray, threshold1, threshold2)
-    kernel = np.ones((5, 5))
-    imgDil = cv2.dilate(imgCanny, kernel, iterations=1)
-    getContours(imgDil, imgContour)
-    display(imgContour)
-
-    ################# FLIGHT
-    if startCounter == 0:
-        me.takeoff()
-        startCounter = 1
-
-    if dir == 1:
-        me.yaw_velocity = -60
-    elif dir == 2:
-        me.yaw_velocity = 60
-    elif dir == 3:
-        me.up_down_velocity = 60
-    elif dir == 4:
-        me.up_down_velocity = -60
-    else:
-        me.left_right_velocity = 0;
-        me.for_back_velocity = 0;
-        me.up_down_velocity = 0;
+    if not faces:
+        # If no faces detected, hover in place
+        me.left_right_velocity = 0
+        me.for_back_velocity = 0
+        me.up_down_velocity = 0
         me.yaw_velocity = 0
-    # SEND VELOCITY VALUES TO TELLO
-    if me.send_rc_control:
-        me.send_rc_control(me.left_right_velocity, me.for_back_velocity, me.up_down_velocity, me.yaw_velocity)
-    print(dir)
+    else:
+        # Get the first detected face (you can modify this to track multiple faces)
+        face = faces[0]
 
-    stack = stackImages(0.9, ([img, result], [imgDil, imgContour]))
-    cv2.imshow('Horizontal Stacking', stack)
+        # Get the facial landmarks
+        shape = predictor(gray, face)
+        shape = face_utils.shape_to_np(shape)
 
+        # Calculate the center of the face (you can use different points for tracking)
+        cx = int((shape[0][0] + shape[16][0]) / 2)
+
+        # Calculate the direction to move the drone to keep the face in the center
+        if cx < int(width / 2) - deadZone:
+            cv2.putText(imgContour, " GO LEFT ", (20, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 3)
+            me.left_right_velocity = -30
+        elif cx > int(width / 2) + deadZone:
+            cv2.putText(imgContour, " GO RIGHT ", (20, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 3)
+            me.left_right_velocity = 30
+        else:
+            # Face is in the desired position, hover in place
+            me.left_right_velocity = 0
+
+    # Continue flying up until reaching 100cm
+    if me.get_height() < 100:
+        me.up_down_velocity = 20  # Adjust the velocity as needed
+    else:
+        me.up_down_velocity = 0
+
+    # Display the image with tracking information
+    cv2.imshow('Face Tracking', imgContour)
+
+    # Send velocity commands to the Tello
+    me.send_rc_control(me.left_right_velocity, me.for_back_velocity, me.up_down_velocity, me.yaw_velocity)
+
+    # Break the loop when 'keys pressed' is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         me.land()
         break
 
+    if cv2.waitKey(1) & 0xFF == ord('w'):
+        me.move_forward(30)
+        break
+
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        me.move_back(30)
+        break
+
+    if cv2.waitKey(1) & 0xFF == ord('a'):
+        me.move_left(30)
+        break
+
+    if cv2.waitKey(1) & 0xFF == ord('d'):
+        me.move_right(30)
+        break
+
+# Clean up
 cv2.destroyAllWindows()
